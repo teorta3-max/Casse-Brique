688f58e1cf659b3ae47b8afbd4b30a042dd9d79b
Flo <teorta3@gmail.com>
2025-12-08 14:40:54 +0100

    essaie si sa marche pas on est mort

--- maincassebrique.c
+++ maincassebrique.c
@@ -1,24 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "cassebrique.h"
 
 int main()
 {
     /*Initialisation des valeurs*/
-    int loop = 1;
     char grille[30][50];
     int dep_x = 0; // valeur de déplacement horizontale
     int dep_y = 0; // verticale
+    int posPlat_x; // position platforme
     int posPlat_y;
-    int posBall_x = 25; // position Balle
-    int posBall_y = 26;
-    int posPlat_x = 23;
+    int posBall_x; // position Balle
+    int posBall_y;
 
     remplirTableau(grille);
     TableauDeBase(grille);
-    while (loop == 1)
-    {
-        system("cls");
-        updateTableau(grille, &posBall_x, &posBall_y, posPlat_x);
-        afficherTableau(grille);
-        Sleep(100);
-    }
+    afficherTableau(grille);
+
 }
