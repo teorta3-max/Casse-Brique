922633e2cbaab3d0602680f53417e38d77b6d0ea
Flo <teorta3@gmail.com>
2025-12-08 13:27:44 +0100

    erreur

--- maincassebrique.c
+++ maincassebrique.c
@@ -1,32 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <conio.h>
-#include <windows.h>
 #include "cassebrique.h"
 
-int main() {
+int main()
+{
     char grille[30][50];
-    int posPlat_x = 23;
-    int posPlat_y = 27;
-    int posBall_x = 25;
-    int posBall_y = 26;
-    int fin = 0;
+    int dep_x = 0; 
+    int dep_y = 0; 
+    int posPlat_x; 
+    int posPlat_y;
+    int posBall_x; 
+    int posBall_y;
 
-    srand(time(NULL));
-
-    grille[posBall_y][posBall_x] = 'O';
+    remplirTableau(grille);
+    TableauDeBase(grille);
     afficherTableau(grille);
 
-    while (fin == 0) {
-        
-        updateTableau(grille, posBall_x, posBall_y, posPlat_x);
-        system("cls");
-        afficherTableau(grille);
-        action(posPlat_x,fin);
-        Sleep(100);
+    if (input() == "q")
+    {
+        posPlat_x = -1;
     }
-
-    return 0;
+    if (input() == "d")
+    {
+        posPlat_x = +1;
     }
 }
