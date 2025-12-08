bad33901123137585cbd937b7d1ee45c0a652011
Flo <teorta3@gmail.com>
2025-12-08 12:24:28 +0100

    essaie

--- cassebrique.h
+++ cassebrique.h
@@ -4,11 +4,8 @@
 
 void remplirTableau(char tab[30][50]);
 void afficherTableau(char tab[30][50]);
-<<<<<<< HEAD
-void updateTableau(char tab[30][50], int posplat_X, int postplatY);
-=======
 void updateTableau(char tab[30][50] ,int posplat_X ,int postplatY);
->>>>>>> e6667bfea5c11cc07152af1f73bdeb89bd63022c
 void TableauDeBase(char tab[30][50]);
+void updateTableau(char tab[30][50], int posballe_X, int posballe_Y);
 char input();
 #endif
