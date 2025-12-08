b2a07368d57c7a4ba3b3a3d255da21696aac7d53
Flo <teorta3@gmail.com>
2025-12-08 11:28:23 +0100

    a

--- cassebrique.h
+++ cassebrique.h
@@ -4,4 +4,6 @@
 void remplirTableau (char tab[30][50]);
 void afficherTableau(char tab[30][50]);
 void TableauDeBase (char tab[30][50]);
+void updateTableau(char tab[30][50] ,int posplat_X ,int postplatY)
+
 #endif
