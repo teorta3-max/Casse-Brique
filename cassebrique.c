ca645eefac63874c890452c984306a3143c07688
Flo <teorta3@gmail.com>
2025-12-08 10:11:28 +0100

    remplirtableau

--- cassebrique.c
+++ cassebrique.c
@@ -3,3 +3,11 @@
 #include <string.h>
 #include <time.h>
 #include "cassebrique.h"
+
+void remplirTableau (char tab[50][30]){
+    for (int i ; i < 50; i++){
+        for (int j ; j < 30; j++){
+            tab[i][j] = ' ';
+        }
+    }
+}
