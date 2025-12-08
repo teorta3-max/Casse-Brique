bf97a4681c74b3caae284227c2b105101f43fd23
Adam Jakubowski <adam.jakubowski@student.junia.com>
2025-12-08 11:41:49 +0100

    fix kbhit

--- cassebrique.h
+++ cassebrique.h
@@ -4,10 +4,7 @@
 
 void remplirTableau(char tab[30][50]);
 void afficherTableau(char tab[30][50]);
-<<<<<<< HEAD
-=======
-void updateTableau(char tab[30][50] ,int posplat_X ,int postplatY);
->>>>>>> dee59fa143c2703adfc0b79fd79447ecae7b75d2
+void updateTableau(char tab[30][50], int posplat_X, int postplatY);
 void TableauDeBase(char tab[30][50]);
 char input();
 #endif
