fb4ee675051b3951307b3c8d2b432416506b4f57
Flo <teorta3@gmail.com>
2025-12-08 12:29:13 +0100

    Essaie avec plus

--- cassebrique.c
+++ cassebrique.c
@@ -4,6 +4,11 @@
 #include <time.h>
 #include "cassebrique.h"
 
+int balleX = 25;
+int balleY = 26;
+int dx = -1;
+int dy = -1;
+
 void remplirTableau(char tab[30][50])
 {
     for (int i = 0; i < 30; i++)
@@ -62,28 +67,30 @@ char input()
     return 0; // sinon renvoie rien
 }
 
-void updateTableau(char tab[30][50], int posballe_X, int posballe_Y){
-    int balleX = 25;
-    int balleY = 26;
-    int dx = -1;
-    int dy = -1;
+void updateTableau(char tab[30][50], int posballe_X, int posballe_Y,int posPlanche){
     tab[balleY][balleX] = ' ';
 
     balleX += dx;
     balleY += dy;
 
-    if (balleX <= 1 || balleX >= 48)
+    if (balleX <= 1 || balleX >= 48){
         dx = -dx;
+    }
+        
 
-    if (balleY <= 1)
+    if (balleY <= 1){
         dy = -dy;
+    }
+        
 
-    if (balleY == 26 && balleX >= 23 && balleX <= 23 + 4)
-    {
+    if (balleY == 26 && balleX >= posPlanche && balleX <= posPlanche + 4){
         dy = -1;
-
-        if (balleX == 23) dx = -1;
-        else if (balleX == 23 + 4) dx = 1;
+        if (balleX == posPlanche){
+            dx = -1;
+        }
+        else if (balleX == posPlanche+4){
+            dx = 1;
+        }
     }
 
     tab[balleY][balleX] = 'O';
