1cc55080cd4a76d366f7bc92037bf8bf9ee21054
Adam Jakubowski <adam.jakubowski@student.junia.com>
2025-12-08 10:15:20 +0100

    Initialisation des valeurs fix

--- maincassebrique.c
+++ maincassebrique.c
@@ -2,3 +2,17 @@
 #include <stdlib.h>
 #include <time.h>
 #include "cassebrique.h"
+
+int main()
+{
+    /*Initialisation des valeurs*/
+    char grille[50][30];
+    int dep_x = 0; // valeur de déplacement horizontale
+    int dep_y = 0; // verticale
+    int posPlat_x; // position platforme
+    int posPlat_y;
+    int posBall_x; // position Balle
+    int posBall_y;
+
+    remplirTableau(grille);
+}
