fc0a539c69921caec20153212a42f4a3a713c646
Flo <teorta3@gmail.com>
2025-12-08 12:41:35 +0100

    Essait presque terminee sans while

--- cassebrique.c
+++ cassebrique.c
@@ -9,8 +9,7 @@ int balleY = 26;
 int dx = -1;
 int dy = -1;
 
-void remplirTableau(char tab[30][50])
-{
+void remplirTableau(char tab[30][50]){
     for (int i = 0; i < 30; i++)
     {
         for (int j = 0; j < 50; j++)
@@ -20,8 +19,7 @@ void remplirTableau(char tab[30][50])
     }
 }
 
-void afficherTableau(char tab[30][50])
-{
+void afficherTableau(char tab[30][50]){
     for (int i = 0; i < 30; i++)
     {
         for (int j = 0; j < 50; j++)
@@ -32,65 +30,52 @@ void afficherTableau(char tab[30][50])
     }
 }
 
-void TableauDeBase(char tab[30][50])
-{
-    tab[26][25] = 'O';
+void TableauDeBase(char tab[30][50]){
     for (int i = 23; i < 28; i++)
-    {
         tab[27][i] = '=';
-    }
+
     for (int i = 1; i < 3; i++)
-    {
         for (int j = 0; j < 50; j++)
-        {
             tab[i][j] = '#';
-        }
-    }
+
     for (int i = 0; i < 50; i++)
     {
         tab[29][i] = '_';
-        tab[0][i] = '_';
+        tab[0][i]  = '_';
     }
+
     for (int i = 1; i < 30; i++)
     {
-        tab[i][0] = '|';
+        tab[i][0]  = '|';
         tab[i][49] = '|';
     }
 }
 
 char input()
 {
-    if (_kbhit()) // lit l'input de l'utilisateur
-    {
-        return (char)_getch(); // le renvoie
-    }
-    return 0; // sinon renvoie rien
+    if (_kbhit())
+        return (char)_getch();
+    
+    return 0;
 }
 
-void updateTableau(char tab[30][50], int posballe_X, int posballe_Y,int posPlanche){
+void updateTableau(char tab[30][50], int posballe_X, int posballe_Y, int posPlanche){
     tab[balleY][balleX] = ' ';
 
     balleX += dx;
     balleY += dy;
 
-    if (balleX <= 1 || balleX >= 48){
+    if (balleX <= 1 || balleX >= 48)
         dx = -dx;
-    }
-        
 
-    if (balleY <= 1){
+    if (balleY <= 1)
         dy = -dy;
-    }
-        
 
-    if (balleY == 26 && balleX >= posPlanche && balleX <= posPlanche + 4){
+    if (balleY == 26 && balleX >= posPlanche && balleX <= posPlanche + 4)
+    {
         dy = -1;
-        if (balleX == posPlanche){
-            dx = -1;
-        }
-        else if (balleX == posPlanche+4){
-            dx = 1;
-        }
+        if (balleX == posPlanche)         dx = -1;
+        else if (balleX == posPlanche+4)  dx = 1;
     }
 
     tab[balleY][balleX] = 'O';
--- maincassebrique.c
+++ maincassebrique.c
@@ -1,23 +1,29 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include "cassebrique.h"
 
-int main()
-{
-    /*Initialisation des valeurs*/
+int main() {
+    /* Initialisation des valeurs */
     char grille[30][50];
-    int dep_x = 0; // valeur de déplacement horizontale
-    int dep_y = 0; // verticale
-    int posPlat_x; // position platforme
-    int posPlat_y;
-    int posBall_x; // position Balle
-    int posBall_y;
+    int posPlat_x = 23; // position initiale de la plateforme
+    int posPlat_y = 27; // ligne de la plateforme
+    int posBall_x = 25; // position initiale de la balle
+    int posBall_y = 26;
 
-    /*setup du tableau*/
+    /* Setup du tableau */
     remplirTableau(grille);
     TableauDeBase(grille);
+    
+    // Placer la balle initialement
+    grille[posBall_y][posBall_x] = 'O';
+
     afficherTableau(grille);
-    updateTableau(grille , posBall_x , posBall_y)
+
+    // Exemple de mise à jour
+    updateTableau(grille, posBall_x, posBall_y, posPlat_x);
+
+    afficherTableau(grille);
+
+    return 0;
 }
