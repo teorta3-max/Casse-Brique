88566a98ba8253dd2bed1cf536bbd6b5945163a1
Adam Jakubowski <adam.jakubowski@student.junia.com>
2025-12-08 15:24:50 +0100

    analyze input quit

--- cassebrique.c
+++ cassebrique.c
@@ -99,15 +99,19 @@ char input()
     return 0;
 }
 
-int analyze_input(char input, int pos)
+int analyze_input(char input, int pos, int loop)
 {
-    if (input == 'q' && pos != 4 || input == 'a' && pos != 4)
+    if (input == 'q' && pos != 4 || input == 'a' && pos != 3)
     {
         return +1;
     }
     else if (input == 'd' && pos != 46)
     {
         return -1;
+    }
+    else if (input == 's')
+    {
+        loop = 0;
     }
     else
     {
