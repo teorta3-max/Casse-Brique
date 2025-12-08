7d573a37650397ae835a62b35dc73fc304398ab3
Adam Jakubowski <adam.jakubowski@student.junia.com>
2025-12-08 11:27:17 +0100

    input dans header

--- cassebrique.h
+++ cassebrique.h
@@ -1,7 +1,8 @@
 #ifndef cassebrique_h
 #define cassebrique_h
 
-void remplirTableau (char tab[30][50]);
+void remplirTableau(char tab[30][50]);
 void afficherTableau(char tab[30][50]);
-void TableauDeBase (char tab[30][50]);
+void TableauDeBase(char tab[30][50]);
+char input();
 #endif
