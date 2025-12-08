0a99deb54a0432dcb2a902c205e38a52c100038d
Flo <teorta3@gmail.com>
2025-12-08 10:55:08 +0100

    Ini

--- cassebrique.c
+++ cassebrique.c
@@ -4,32 +4,32 @@
 #include <time.h>
 #include "cassebrique.h"
 
-void remplirTableau (char tab[50][30]){
-    for (int i ; i < 50; i++){
-        for (int j ; j < 30; j++){
+void remplirTableau(char tab[50][30]){
+    for (int i = 0; i < 50; i++){
+        for (int j = 0; j < 30; j++){
             tab[i][j] = ' ';
         }
     }
 }
 
-void afficherTableau (char tab[50][30]){
-    for (int i ; i < 50; i++){
-        for (int j ; j < 30; j++){
-            printf("%c",tab[i][j]);
+void afficherTableau(char tab[50][30]){
+    for (int i = 0; i < 50; i++){
+        for (int j = 0; j < 30; j++){
+            printf("%c", tab[i][j]);
         }
         printf("\n");
     }
 }
 
-void TableauDeBase (char tab[50][30]){
+void TableauDeBase(char tab[50][30]){
     tab[45][15] = 'O';
-    for (int i =13;i < 18; i++){
+    for (int i = 13; i < 18; i++){
         tab[46][i] = '=';
     }
-    char brik = "|###|";
-    for (int i = 0;i < 2 ;i++){
-        for (int j ; j < 30; j += 5){
-            tab[i][j] = brik;
+    for (int i = 0; i < 2; i++){     
+        for (int j = 0; j < 30; j++){    
+            if (j % 5 == 0)                
+                tab[i][j] = '#';
         }
     }
 }
