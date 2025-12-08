7e75f68ed9c816fda26c280188013c4db65aefc8
Mauriac <anatole.mauriac@gmail.com>
2025-12-08 13:46:48 +0100

    tout dans le .h

--- cassebrique.c
+++ cassebrique.c
@@ -1,7 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
 #include "cassebrique.h"
 
 int dx = -1;
--- maincassebrique.c
+++ maincassebrique.c
@@ -1,7 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <windows.h>
 #include "cassebrique.h"
 
 int main()
