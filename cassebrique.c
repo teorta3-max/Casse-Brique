47cf29199b1d8e0789b0c5035b1ffd6a057ad95f
Mauriac <anatole.mauriac@gmail.com>
2025-12-08 13:37:49 +0100

    pos balle bien fait

--- cassebrique.c
+++ cassebrique.c
@@ -4,8 +4,6 @@
 #include <time.h>
 #include "cassebrique.h"
 
-int balleX = 25;
-int balleY = 26;
 int dx = -1;
 int dy = -1;
 
@@ -60,28 +58,28 @@ char input(){
     return 0;
 }
 
-void updateTableau(char tab[30][50], int posballe_X, int posballe_Y, int posPlanche){
-    tab[balleY][balleX] = ' ';
+void updateTableau(char tab[30][50], int posBalle_x, int posBalle_y, int posPlanche){
+    tab[posBalle_y][posBalle_x] = ' ';
 
-    balleX += dx;
-    balleY += dy;
+    posBalle_x += dx;
+    posBalle_y += dy;
 
-    if (balleX <= 1 || balleX >= 48)
+    if (posBalle_x <= 1 || posBalle_x >= 48)
         dx = -dx;
 
-    if (balleY <= 1)
+    if (posBalle_y <= 1)
         dy = -dy;
 
-    if (balleY == 26 && balleX >= posPlanche && balleX <= posPlanche + 4)
+    if (posBalle_y == 26 && posBalle_x >= posPlanche && posBalle_x <= posPlanche + 4)
     {
         dy = -1;
-        if (balleX == posPlanche)
+        if (posBalle_x == posPlanche)
             dx = -1;
-        else if (balleX == posPlanche + 4)
+        else if (posBalle_x == posPlanche + 4)
             dx = 1;
     }
 
-    tab[balleY][balleX] = 'O';
+    tab[posBalle_y][posBalle_x] = 'O';
 }
 
 void action(int posPlat_x, int fin){
