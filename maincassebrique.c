19ddba4e75482bd2f1fb40e7b0cd0619a87b5500
Flo <teorta3@gmail.com>
2025-12-08 13:14:20 +0100

    Peut etre pour le déplacement de la plateforme

--- maincassebrique.c
+++ maincassebrique.c
@@ -5,7 +5,6 @@
 #include <windows.h>
 #include "cassebrique.h"
 
-<<<<<<< HEAD
 int main()
 {
     /*Initialisation des valeurs*/
@@ -18,36 +17,7 @@ int main()
     int posBall_x; // position Balle
     int posBall_y;
     int dep;
-=======
-int main() {
-    char grille[30][50];
-    int posPlat_x = 23;
-    int posPlat_y = 27;
-    int posBall_x = 25;
-    int posBall_y = 26;
-    int fin = 0;
-
-    srand(time(NULL));
->>>>>>> d1ea0f85385d6b636f93519a57bf136c54ee2bea
 
     remplirTableau(grille);
     TableauDeBase(grille);
-<<<<<<< HEAD
-=======
-    grille[posBall_y][posBall_x] = 'O';
-    afficherTableau(grille);
-
-    while (fin == 0) {
-        action(posPlat_x,fin);
-
-        updateTableau(grille, posBall_x, posBall_y, posPlat_x);
-
-        system("cls");
-        afficherTableau(grille);
-
-        Sleep(100);
-    }
-
-    return 0;
->>>>>>> d1ea0f85385d6b636f93519a57bf136c54ee2bea
 }
